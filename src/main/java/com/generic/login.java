cedf641c556f485fdab15662468d4f11d4b00edf
ThiccThighNekos <aornob2000@gmail.com>
2023-11-17 18:08:33 -0700

    first commit

---
+++ src/main/java/com/generic/login.java
@@ -0,0 +1,15 @@
+package com.generic;
+
+public class login {
+	
+int b=20;//name b-->Global--> instance
+static int c=40; //name =c ==> Global==> static
+	
+	
+ public static void main(String[] args) {
+	
+
+
+     }
+	
+}
